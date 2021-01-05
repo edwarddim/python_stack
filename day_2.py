dcbe5bb152a0ec93eb860d6921aaf5fd69247b84
Edward <eddieim@gmail.com>
2021-01-05 16:58:04 -0700

    W1D2 afternoon

--- day_2.py
+++ day_2.py
@@ -1,3 +1,4 @@
+
 # OOP
 class Person:
     # ATTRIBUTES(CHARACTERISTICS)
@@ -25,15 +26,41 @@ class Person:
 # CONSTRUCTOR METHOD AND OTHER METHODS
 
 # INSANTIATING CLASS
-user1 = Person(4, "Black", "Edward", 30) # INSTANTIATING A NEW OBJECT
-user2 = Person(4, "Blue", "John", 34) # INSTANTIATING A NEW OBJECT
+# user1 = Person(4, "Black", "Edward", 30) # INSTANTIATING A NEW OBJECT
+# user2 = Person(4, "Blue", "John", 34) # INSTANTIATING A NEW OBJECT
 # print(user1.limbs)
 # print(user1.name)
 # print(user1.age)
 # user1.introduction()
 # user2.introduction()
-print(user1.hair_color)
-user1.birthday()
-user1.hairColorChange("Red")
-print(user1.hair_color)
-print(user1.age)
+# print(user1.hair_color)
+# user1.birthday()
+# user1.hairColorChange("Red")
+# print(user1.hair_color)
+# print(user1.age)
+
+
+# DEFAULT PARAMETERS & NAMED ARGUMENTS
+class User:
+    def __init__(self, name, int_rate, amount):
+        self.name = name
+        self.account = BankAccount(int_rate, amount)
+    
+    def deposit(self, amount):
+        self.account.deposit(amount)
+
+class BankAccount:
+    def __init__(self, int_rate, balance):
+        self.int_rate = int_rate
+        self.balance = balance
+    def deposit(self, amount):
+        print(f"Depositing {amount} into balance")
+    
+    def withdraw(self, amount):
+        print(f"Withdrawing {amount} from balance")
+
+user1 = User("Edward", 0.1, 1000)
+user1.deposit(400)
+user1.deposit(400)
+user1.deposit(400)
+user1.deposit(400)
