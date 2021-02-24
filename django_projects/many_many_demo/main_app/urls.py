8ccfb3bfb4d14c7e143487596eb294a112b0d77c
Edward <eddieim@gmail.com>
2021-02-23 17:16:23 -0700

    W3D2 afternoon

--- django_projects/many_many_demo/main_app/urls.py
+++ django_projects/many_many_demo/main_app/urls.py
@@ -2,5 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+# ------------ CREATE ------------------------------- #
+    path("", views.index),
+    path("user/new", views.createUser),
+# ------------ CREATE ------------------------------- #
+# ------------ READ ONE USER ------------------------ #
+    path("users/<int:user_id>", views.user),
+# ------------ READ ONE USER ------------------------ #
+# ------------ USER JOINING A GROUP ----------------- #
+    path("users/add_group", views.joinGroup)
+# ------------ USER JOINING A GROUP ----------------- #
+
 ]
--- django_projects/many_many_demo/main_app/views.py
+++ django_projects/many_many_demo/main_app/views.py
@@ -1,5 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User, Group
 
 # Create your views here.
+# ------------ CREATE ------------------------------- #
 def index(request):
-    return render(request, "index.html")
+    context = {
+        "all_users" : User.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def createUser(request):
+    # 1. GRAB POST INFORMATION AND CREATE A NEW USER
+    new_user = User.objects.create(
+        name = request.POST['name'],
+        email = request.POST['email'],
+        password = request.POST['password']
+    )
+    print(new_user.id)
+    # 2. REDIRECT TO INDEX ROUTE
+    return redirect("/")
+# ------------ CREATE ------------------------------- #
+
+# ------------ READ ONE USER ------------------------ #
+def user(request,user_id):
+    print(f'User ID: {user_id}')
+    context = {
+        "one_user" : User.objects.get(id = user_id),
+        "all_groups" : Group.objects.all()
+    }
+    return render(request, "user.html", context)
+# ------------ READ ONE USER ------------------------ #
+
+# ------------ USER JOINING A GROUP ------------------------ #
+def joinGroup(request):
+    user_id = request.POST['user_id']
+    # 1. GRAB THE USER FROM DB
+    user = User.objects.get(id = user_id)
+    # 2. GRAB THE GROUP FROM DB
+    group = Group.objects.get(id = request.POST['group_id'])
+    # 3. CREATE RELATIONSHIP BETWEEN THE USER AND GROUP
+    user.groups.add(group) # OR group.members.add(user)
+
+    # 4. REDIRECT /users/ID#
+    return redirect(f"/users/{user_id}")
+# ------------ USER JOINING A GROUP ------------------------ #
+
